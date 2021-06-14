90472cbaf8770ce998f1b36e02fc8ccbe65dfc6a
Márta Márton <daniel.marton.marta@gmail.com>
2021-06-14 09:18:06 +0200

    adding pring initializer stuff

---
+++ src/main/java/hu/flow/academy/kappaspringexam/KappaSpringExamApplication.java
@@ -0,0 +1,13 @@
+package hu.flow.academy.kappaspringexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaSpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaSpringExamApplication.class, args);
+	}
+
+}
